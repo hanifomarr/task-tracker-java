06cf994ef3d8f1c62128b8fc2d606be8f97d7f30
Hanif Omar <hanif.omarr@gmail.com>
2026-01-06 15:19:34 +0800

    feat(domain): add task and tasklist entity

---
+++ src/main/java/com/hanifomarr/tasks/domain/entities/TaskPriority.java
@@ -0,0 +1,8 @@
+package com.hanifomarr.tasks.domain.entities;
+
+public enum TaskPriority {
+    HIGH,
+    MEDIUM,
+    LOW
+}
+
---
+++ src/main/java/com/hanifomarr/tasks/domain/entities/TaskStatus.java
@@ -0,0 +1,6 @@
+package com.hanifomarr.tasks.domain.entities;
+
+public enum TaskStatus {
+    OPEN,
+    CLOSED
+}

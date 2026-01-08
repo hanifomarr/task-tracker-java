1c277c87ba0ca4430496850dabf6336cb8d63ce3
Hanif Omar <hanif.omarr@gmail.com>
2026-01-08 17:33:09 +0800

    feat(task): implement create and fetch task

---
+++ src/main/java/com/hanifomarr/tasks/controllers/TaskController.java
@@ -0,0 +1,36 @@
+package com.hanifomarr.tasks.controllers;
+
+import com.hanifomarr.tasks.domain.dto.TaskDto;
+import com.hanifomarr.tasks.domain.entities.Task;
+import com.hanifomarr.tasks.mappers.TaskMapper;
+import com.hanifomarr.tasks.services.TaskService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping(path = "/task-lists/{task_list_id}/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+    private final TaskMapper taskMapper;
+
+    public TaskController(TaskService taskService, TaskMapper taskMapper) {
+        this.taskService = taskService;
+        this.taskMapper = taskMapper;
+    }
+
+    @GetMapping
+    public List<TaskDto> listTasks(@PathVariable("task_list_id") UUID taskListId) {
+        return taskService.listTasks(taskListId).stream().map(taskMapper::toDto).toList();
+    }
+
+    @PostMapping
+    public TaskDto creteTask(@PathVariable("task_list_id") UUID taskListId,
+                             @RequestBody TaskDto taskDto) {
+
+        Task createdTask = taskService.createTask(taskListId, taskMapper.fromDto(taskDto));
+        return taskMapper.toDto(createdTask);
+    }
+}
---
+++ src/main/java/com/hanifomarr/tasks/services/impl/TaskServiceImpl.java
@@ -0,0 +1,62 @@
+package com.hanifomarr.tasks.services.impl;
+
+import com.hanifomarr.tasks.domain.entities.Task;
+import com.hanifomarr.tasks.domain.entities.TaskList;
+import com.hanifomarr.tasks.domain.entities.TaskPriority;
+import com.hanifomarr.tasks.domain.entities.TaskStatus;
+import com.hanifomarr.tasks.repositories.TaskListRepository;
+import com.hanifomarr.tasks.repositories.TaskRepository;
+import com.hanifomarr.tasks.services.TaskService;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+    private final TaskListRepository taskListRepository;
+
+    public TaskServiceImpl(TaskRepository taskRepository, TaskListRepository taskListRepository) {
+        this.taskRepository = taskRepository;
+        this.taskListRepository = taskListRepository;
+    }
+
+    @Override
+    public List<Task> listTasks(UUID taskListId) {
+        return taskRepository.findByTaskListId(taskListId);
+    }
+
+    @Override
+    public Task createTask(UUID taskListId, Task task) {
+        if (null != task.getId()) {
+            throw new IllegalArgumentException("Task has been created");
+        }
+
+        if (null == task.getTitle() || task.getTitle().isBlank()) {
+            throw new IllegalArgumentException("Task title is required");
+        }
+
+        TaskStatus taskStatus = TaskStatus.OPEN;
+        TaskPriority taskPriority = Optional.ofNullable(task.getPriority()).orElse(TaskPriority.MEDIUM);
+        TaskList taskList = taskListRepository.findById(taskListId).orElseThrow(() -> new IllegalArgumentException("Invalid Task List"));
+        LocalDateTime now = LocalDateTime.now();
+
+        return taskRepository.save(new Task(
+                null,
+                task.getTitle(),
+                task.getDescription(),
+                task.getDueDate(),
+                taskStatus,
+                taskPriority,
+                taskList,
+                now,
+                now
+        ));
+    }
+
+
+}
